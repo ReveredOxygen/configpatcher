13be0b461c9ebca11b19de45a0a84917d0e75a46
ReveredOxygen <reveredoxygen@gmail.com>
2024-12-26 04:28:59 -0500

    Support deleting non-empty directories with deleteFile patch type

--- src/main/java/io/github/reveredoxygen/configpatcher/patches/DeleteFilePatch.java
+++ src/main/java/io/github/reveredoxygen/configpatcher/patches/DeleteFilePatch.java
@@ -1,11 +1,33 @@
 package io.github.reveredoxygen.configpatcher.patches;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class DeleteFilePatch {
     public void apply(Path target) throws IOException {
-        Files.delete(target);
+        // Recursively delete if target is a directory
+        if (Files.isDirectory(target)) {
+            // Code taken from https://stackoverflow.com/a/27917071
+            Files.walkFileTree(target, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    super.postVisitDirectory(dir, exc);
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } else {
+            Files.delete(target);
+        }
     }
 }
